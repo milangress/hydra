7d8abaa8212e71473b53be009b7627e6ac9b29fd
Milan G <dev@milangress.de>
2025-03-23 12:31:55 +0100

    Update build and dev scripts in package.json; add preview script and enhance info modal

--- src/views/info.js
+++ src/views/info.js
@@ -8,6 +8,7 @@ const link = (url) => `href=${url} target=_blank`
 
 const infoText = (t) => html`
 <div class="modal-content">
+<div class="dev"> Hydra Experimental  Milan Gress • Feats: code-completions </div>
 <h1>${t('info.title')}</h1>
 <h3>${t('info.subtitle')}</h3>
   <br> ///////////////////////////////////////////////////////////<br>
@@ -56,12 +57,14 @@ export default function mainView(state, emit) {
 
 const header = html`<div></div>`
   const content = html`
+  <div>hello</div>
   ${state.showInfo && langArray.length > 1 ? html`<div style="display:flex;flex-wrap:wrap">${langArray.map(([key, val]) => html`
   <div class="language-select" onclick=${() => emit('set language', key)}>${val}</div>
-  `)}</div>` : html`<div></div>`} 
+  `)}</div>` : html`<div> </div>`} 
   ${infoText(t)}
   `
   const windowContent = !state.showExtensions ? { content: content, header: header } : extensionInfo(state, emit)
+  
   return modalWindow(windowContent, state, emit)
 }
 
